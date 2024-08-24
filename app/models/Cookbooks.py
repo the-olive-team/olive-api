08ac6200a41f7cec2f0634adee5b896146a821ab
7Woos <swenwu96@gmail.com>
2024-08-24 15:58:39 -0700

    Initial cookbook API model v3

--- app/models/Cookbooks.py
+++ app/models/Cookbooks.py
@@ -1,9 +1,6 @@
 import uuid
 from sqlmodel import Field, Relationship, SQLModel
 
-
-
-# Cookbook Table
 class Cookbook(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     owner_id: int = Field(foreign_key="user.id", unique=True)
@@ -11,10 +8,10 @@ class Cookbook(SQLModel, table=True):
     thumbnail: str | None = Field(default=None)
     title: str | None = Field(default=None)
     description: str | None = Field(default=None)
-    # 4th uuid generator
     uuid: str = Field(default_factory=lambda: str(uuid.uuid4()), unique=True)
 
     owner: User = Relationship(back_populates="cookbooks")
     recipes: list["CookbookRecipe"] = Relationship(back_populates="cookbook")
     permissions: list["CookbookPermissions"] = Relationship(back_populates="cookbook")
     likes: list["CookbookLike"] = Relationship(back_populates="cookbook")
+    

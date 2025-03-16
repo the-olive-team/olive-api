007a4286210e96a88c4c1b5463a5c6b03df44cf0
djbarajas <danielbarajas21@gmail.com>
2025-03-16 08:28:57 -0700

    cleanup cookbooks create

--- app/api/routes/cookbooks.py
+++ app/api/routes/cookbooks.py
@@ -11,12 +11,12 @@ from app.models import CookbookPublic, Cookbook, CookbookCreate
 router = APIRouter()
 
 
-@router.post("/cookbooks", response_model=CookbookPublic)
+@router.post("/", response_model=CookbookPublic)
 def create_cookbook(
     *, session: SessionDep, current_user: CurrentUser, item_in: CookbookCreate
 ) -> Any:
     """
-    Create new item.
+    Create new cookbook.
     """
     cookbook = Cookbook.model_validate(item_in, update={"owner_id": current_user.id})
     session.add(cookbook)

1e9df4938e747f7e8b602b63f3313c4ce2f0aab9
djbarajas <danielbarajas21@gmail.com>
2025-03-23 08:45:38 -0700

    add cookbooks endpoints

--- app/api/routes/cookbooks.py
+++ app/api/routes/cookbooks.py
@@ -1,3 +1,6 @@
+
+import uuid
+from datetime import timedelta
 from typing import Any
 
 from fastapi import APIRouter
@@ -27,7 +30,7 @@ def get_cookbook(
     *, session: SessionDep, current_user: CurrentUser, offset: int = 0, limit: int = 100
 ) -> Any:
     """
-    Create new cookbook.
+    Get cookbooks.
     """
     count_statement = (
         select(func.count())
@@ -42,5 +45,48 @@ def get_cookbook(
         .limit(limit)
     )
     cookbooks = session.exec(statement).all()
-    print(cookbooks)
     return CookbooksPublic(data=cookbooks, count=count)
+
+
+@router.get("/{cookbook_id}", response_model=CookbookPublic)
+def get_cookbook(
+    cookbook_id: uuid.UUID, session: SessionDep, current_user: CurrentUser
+) -> Any:
+    """
+    Get cookbook by id.
+    """
+    cookbook = session.get(Cookbook, cookbook_id)
+    if not cookbook:
+        raise HTTPException(
+            status_code=404,
+            detail="The cookbook was not found",
+        )
+    if cookbook.owner_id != current_user.id:
+        raise HTTPException(
+            status_code=403,
+            detail="The user doesn't have enough privileges",
+        )
+    return cookbook
+
+
+@router.delete("/{cookbook_id}", response_model=CookbookPublic)
+def delete_cookbook(
+    cookbook_id: uuid.UUID, session: SessionDep, current_user: CurrentUser
+) -> Any:
+    """
+    Get cookbook by id.
+    """
+    cookbook = session.get(Cookbook, cookbook_id)
+    if not cookbook:
+        raise HTTPException(
+            status_code=404,
+            detail="The cookbook was not found",
+        )
+    if cookbook.owner_id != current_user.id:
+        raise HTTPException(
+            status_code=403,
+            detail="The user doesn't have enough privileges",
+        )
+    session.delete(cookbook)
+    session.commit()
+    return cookbook
